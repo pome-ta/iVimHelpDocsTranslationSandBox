494c49d3130f52cec0b45e8867d7bfd89f82a5e1
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-12-01 13:41:57 +0900

    update

--- update.py
+++ update.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 import shutil
-import sys
+
+# 事前にiVim 上でこのファイル(`update.py`) を開く
+# iVim でコマンド`:terminal`
+# で、ivish を立ち上げ
+# `echo $VIMRUNTIME > ./.path` で、ランタイムパスを取得
 
 vimruntime_txt = Path('./.path').read_text().strip()
 doc_path = Path(vimruntime_txt, 'doc')
@@ -9,5 +13,4 @@ from_path = Path('./iVimDocsMaster')
 for f in doc_path.iterdir():
   f_name = f.name
   shutil.copy2(Path(doc_path, f_name), Path(from_path, f_name))
-
 
